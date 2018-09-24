e18b0adc8a5d11b42bbee1b80b1a0fa130567a19
stephen <sjw948@gmail.com>
2018-09-24 19:50:30 +0100

    restructure router outlet + view wrapper

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,24 @@
 <template lang="html">
   <div id="app">
-    <router-view></router-view>
+    <div class="view-wrapper">
+      <website-menu></website-menu>
+      <router-view></router-view>
+      <website-footer></website-footer>
+    </div>
   </div>
 </template>
 
 <script>
 
-export default {};
+import WebsiteMenu from './modules/website/components/menu/menu.vue';
+import WebsiteFooter from './modules/website/components/footer/footer.vue';
+
+export default {
+  components: {
+    WebsiteMenu,
+    WebsiteFooter
+  }
+};
 </script>
 
 <style lang="scss">
--- src/modules/website/view-wrapper.vue
+++ src/modules/website/view-wrapper.vue
@@ -1,31 +1,14 @@
-
 <template lang="html">
-  <div class="view-wrapper">
-    <website-menu></website-menu>
-    <website-home-view></website-home-view>
-    <website-footer></website-footer>
-  </div>
+  <website-home-view></website-home-view>
 </template>
 
 <script>
-import WebsiteMenu from './components/menu/menu.vue';
 import WebsiteHomeView from './components/home/home-view.vue';
-import WebsiteFooter from './components/footer/footer.vue';
 
 export default {
 	name: 'WebsiteViewWrapper',
   components: {
-    WebsiteMenu,
     WebsiteHomeView,
-    WebsiteFooter
   }
 };
 </script>
-
-<style lang="scss" scoped>
-  @import '~app-root/styles/variables/index';
-
-  .view-wrapper {
-    position: relative;
-  }
-</style>
