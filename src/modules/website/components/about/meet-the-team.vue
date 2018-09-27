f1d5f3f58afdb0988edfe19cac38f23add109487
stephen <sjw948@gmail.com>
2018-09-27 07:57:57 +0100

    meet the team stylings and overlays

--- src/modules/website/components/about/meet-the-team.vue
+++ src/modules/website/components/about/meet-the-team.vue
@@ -6,24 +6,66 @@
       <p>The experts at our Codeless web design agency have more than 10 years of combined experience in web development and design.</p>
       <div class="team-members row">
         <div class="team-member col col-sm-4">
-          <img src="/assets/img/staff5-400x270.jpg">
+          <div class="image-container">
+            <img src="/assets/img/staff5-400x270.jpg">
+            <div class="image-overlay">
+              <div class="icon">
+                <i class="fab fa-facebook"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-linkedin"></i>
+              </div>
+            </div>
+          </div>
           <div class="caption">
-            <h5>Picture</h5>
-            <p>Picture caption</p>
+            <h5>John Doe</h5>
+            <span>The next CEO</span>
+            <p>Ut elementum adipiscing elit quis viverra. Quisque eu ipsum in justo consectetur adipiscing elit quis.</p>
           </div>
         </div>
         <div class="team-member col col-sm-4">
-          <img src="/assets/img/staff4-400x270.jpg">
+          <div class="image-container">
+            <img src="/assets/img/staff4-400x270.jpg">
+            <div class="image-overlay">
+              <div class="icon">
+                <i class="fab fa-facebook"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-linkedin"></i>
+              </div>
+            </div>
+          </div>
           <div class="caption">
-            <h5>Picture</h5>
-            <p>Picture caption</p>
+            <h5>Jane Doe</h5>
+            <span>CEO</span>
+            <p>Ut elementum adipiscing elit quis viverra. Quisque eu ipsum in justo consectetur adipiscing elit quis.</p>
           </div>
         </div>
         <div class="team-member col col-sm-4">
-          <img src="/assets/img/staff21-400x270.jpg">
+          <div class="image-container">
+            <img src="/assets/img/staff21-400x270.jpg">
+            <div class="image-overlay">
+              <div class="icon">
+                <i class="fab fa-facebook"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-twitter"></i>
+              </div>
+              <div class="icon">
+                <i class="fab fa-linkedin"></i>
+              </div>
+            </div>
+          </div>
           <div class="caption">
-            <h5>Picture</h5>
-            <p>Picture caption</p>
+            <h5>Jeremy Doe</h5>
+            <span>CTO</span>
+            <p>Ut elementum adipiscing elit quis viverra. Quisque eu ipsum in justo consectetur adipiscing elit quis.</p>
           </div>
         </div>
       </div>
@@ -49,6 +91,7 @@ export default {
       font-size: 22px;
       font-weight: 600;
       text-transform: uppercase;
+      text-align: center;
 
       @media(min-width: $screen-md) {
         font-size: 28px;
@@ -68,11 +111,13 @@ export default {
       width: 480px;
       max-width: 80%;
       margin: 0 auto;
+      text-align: center;
     }
   }
 
   .team-members {
     display: flex;
+    margin-top: 35px;
 
     .col {
       margin-bottom: 0;
@@ -80,9 +125,88 @@ export default {
 
     .team-member {
       padding: 0 15px;
+
+      .image-container {
+        position: relative;
+
+        .image-overlay {
+          background: rgba(#333, .7);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          opacity: 0;
+          transition: 0.3s ease all;
+
+          .icon {
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50%;
+            border: 2px solid rgba(#CCCCCC, .7);
+            transform: translateY(50px);
+            margin: 0 10px;
+            transition: 0.3s ease all;
+            opacity: 0;
+
+            i {
+              color: rgba(#CCCCCC, .7);
+              font-size: 24px;
+              padding: 15px;
+              transition: 0.3s ease all;
+            }
+          }
+        }
+      }
+
       .caption {
         background: white;
         padding: 20px;
+
+        h5 {
+          font-weight: 600;
+          font-size: 16px;
+        }
+
+        span {
+          display: inline-block;
+          font-size: 14px;
+          font-weight: 600;
+          color: $brand-blue;
+          margin-bottom: 5px;
+        }
+
+        p {
+          color: $font-grey;
+          font-size: 12px;
+          line-height: 1.45;
+        };
+      }
+
+      &:hover {
+        .image-container {
+          .image-overlay {
+            opacity: 1;
+
+            .icon {
+              opacity: 1;
+              transform: translateY(0);
+
+              &:hover {
+                border-color: rgba(#FFFFFF, .7);
+                i {
+                  color: #FFFFFF;
+                }
+              }
+            }
+          }
+        }
+
       }
     }
   }
