cbb496ab60bcbfdd6469ff54c95cbf4c807a09ca
stephen <sjw948@gmail.com>
2018-09-30 17:02:19 +0100

    faqs container component

---
+++ src/modules/website/components/faq/faqs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="faqs">
+    <div class="container-1100">
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'FAQS',
+  components: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~app-root/styles/variables/index';
+
+  #faqs {
+
+  }
+</style>
--- src/modules/website/components/faq/view.vue
+++ src/modules/website/components/faq/view.vue
@@ -1,16 +1,19 @@
 <template lang="html">
   <div id="faq-view">
     <hero></hero>
+    <faqs></faqs>
   </div>
 </template>
 
 <script>
 import Hero from './hero.vue';
+import Faqs from './faqs.vue';
 
 export default {
   name: 'FAQView',
   components: {
-    Hero
+    Hero,
+    Faqs
   },
   mounted() {
     document.getElementById('website-menu').classList.add('menu-dark');
