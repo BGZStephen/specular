83956872b52e9880ccd6793a9dba8f1f1d571b34
stephen <sjw948@gmail.com>
2018-09-30 11:20:56 +0100

    faq view component + route

---
+++ src/modules/website/components/faq/view.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div id="faq-view">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FAQView',
+  components: {},
+  mounted() {
+    document.getElementById('website-menu').classList.add('menu-dark');
+  }
+};
+</script>
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,9 +1,11 @@
 import HomeView from '../modules/website/components/home/view.vue';
 import AboutView from '../modules/website/components/about/view.vue';
 import ServicesView from '../modules/website/components/services/view.vue';
+import FAQView from '../modules/website/components/faq/view.vue';
 
 export default [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
   { path: '/services', component: ServicesView },
+  { path: '/faq', component: FAQView }
 ];
