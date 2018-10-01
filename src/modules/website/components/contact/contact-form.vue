8e953278953778d74289c929118592fcd80848a0
stephen <sjw948@gmail.com>
2018-10-01 20:35:49 +0100

    contact form component

---
+++ src/modules/website/components/contact/contact-form.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="contact-form">
+    <h5>Get connected</h5>
+    <div class="input-group">
+      <label for="name">Your Name (required)</label>
+      <input id="name" name="name" type="text" class="form-input" required>
+    </div>
+    <div class="input-group">
+      <label for="email">Your Email (required)</label>
+      <input id="email" name="email" type="email" class="form-input">
+    </div>
+    <div class="input-group">
+      <label for="subject">Subject</label>
+      <input id="subject" name="subject" type="text" class="form-input">
+    </div>
+    <div class="input-group">
+      <label for="message">Your message</label>
+      <textarea class="form-input" id="message" name="message" rows="5"></textarea>
+    </div>
+    <button type="button">Send now</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm'  
+}
+</script>
+
+<style lang="scss">
+  @import '~app-root/styles/variables/index';
+
+  #contact-form {
+    h5 {
+      font-size: 22px;
+      font-weight: 600;
+      margin-bottom: 25px;
+      text-transform: uppercase;
+    }
+
+    .input-group {
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px;
+
+      label {
+        color: $font-grey;
+        font-size: 12px;
+        margin-bottom: 3px;
+      }
+
+      input, textarea {
+        border: 2px solid white;
+        font-size: 14px;
+        padding: 10px;
+        transition: 0.3s ease all;
+
+        &:active,
+        &:focus {
+          border: 2px solid $brand-blue;
+          outline: none;
+        }
+      }
+    }
+
+    button {
+      background: none;
+      cursor: pointer;
+      border: 2px solid $font-light-grey;
+      font-size: 12px;
+      font-weight: 600;
+      padding: 10px 35px;
+      text-transform: uppercase;
+      transition: 0.3s ease all;
+
+      &:hover {
+        border: 2px solid black;
+      }
+    }
+  }
+</style>
+
--- src/modules/website/components/contact/view.vue
+++ src/modules/website/components/contact/view.vue
@@ -1,19 +1,40 @@
 <template lang="html">
   <div id="contact-view">
     <hero></hero>
+    <div id="contact-details" class="container-1100">
+      <div class="row">
+        <div class="col col-sm-8">
+          <contact-form></contact-form>
+        </div>
+        <div class="col col-sm-4">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import Hero from './hero.vue';
+import ContactForm from './contact-form.vue';
 
 export default {
 	name: 'ContactView',
   components: {
-    Hero
+    Hero,
+    ContactForm
   },
   mounted() {
     document.getElementById('website-menu').classList.remove('menu-dark');
   }
 };
 </script>
+
+<style lang="scss">
+#contact-view {
+  background: #f5f5f5;
+  #contact-details {
+    padding: 60px 25px;
+  }
+}
+</style>
