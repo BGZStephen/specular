371f452689dfbb8e13095e63b642df4c2a5dcb71
stephen <sjw948@gmail.com>
2018-10-01 19:51:04 +0100

    contact view component

---
+++ src/modules/website/components/contact/view.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="contact-view">
+  </div>
+</template>
+
+<script>
+
+export default {
+	name: 'ContactView',
+  components: {
+
+  },
+  mounted() {
+    document.getElementById('website-menu').classList.remove('menu-dark');
+  }
+};
+</script>
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,10 +2,12 @@ import HomeView from '../modules/website/components/home/view.vue';
 import AboutView from '../modules/website/components/about/view.vue';
 import ServicesView from '../modules/website/components/services/view.vue';
 import FAQView from '../modules/website/components/faq/view.vue';
+import ContactView from '../modules/website/components/contact/view.vue';
 
 export default [
   { path: '/', component: HomeView },
   { path: '/about', component: AboutView },
   { path: '/services', component: ServicesView },
-  { path: '/faq', component: FAQView }
+  { path: '/faq', component: FAQView },
+  { path: '/contact', component: ContactView },
 ];
